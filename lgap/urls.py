8d815a95a093d6ea6ced3f435a5822f812df38ed
unknown <jdbuysse@gmail.com>
2020-03-06 11:00:24 -0500

    messing with media root settings in project level urls

--- lgap/urls.py
+++ lgap/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('nlp.urls')),
+    path('', include('nlp.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
